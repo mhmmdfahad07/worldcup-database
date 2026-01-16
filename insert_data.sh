67a04b81cb4b338398855b973e7f62b32b60ef18
mhmmdfahad07 <mhmmdfahad07@gmail.com>
2026-01-16 19:07:48 +0700

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
-# reset tables
-$PSQL "TRUNCATE games, teams RESTART IDENTITY"
+# Do not change code above this line. Use the PSQL variable above to query your database.
 
-# insert unique teams
-cat games.csv | tail -n +2 | cut -d',' -f3,4 | tr ',' '\n' | sort -u |
-while read TEAM
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  $PSQL "INSERT INTO teams(name) VALUES('$TEAM')"
+  if [[ $YEAR != "year" ]]
+  then
+    # WINNER
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $WINNER_ID ]]
+    then
+      $PSQL "INSERT INTO teams(name) VALUES('$WINNER')"
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # OPPONENT
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')"
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # INSERT GAME
+    $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
+  fi
 done
-
-# insert games
-cat games.csv | tail -n +2 |
-while IFS="," read YEAR ROUND WINNER OPPONENT WGOALS OGOALS
-do
-  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-
-  $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
-  VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WGOALS, $OGOALS)"
-done
-
